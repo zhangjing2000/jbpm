262de66738691ec725689e2b369af9bf1122ba59
pefernan <pefernan@redhat.com>
2013-05-29 16:15:56 +0200

    refactored form rendering to integrate jbpm-form-modeler

---
+++ jbpm-services/jbpm-kie-services/src/main/java/org/jbpm/kie/services/impl/form/provider/FreemakerFormProvider.java
@@ -0,0 +1,11 @@
+package org.jbpm.kie.services.impl.form.provider;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pefernan
+ * Date: 24/05/13
+ * Time: 13:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class FreemakerFormProvider {
+}
